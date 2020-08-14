487b04f22be6bfe1a6977743a2d1620ce150e993
petercolella <peter.joseph.colella@gmail.com>
2020-08-13 22:00:57 -0400

    npm audit fix

---
+++ public/precache-manifest.b1f76ad2c3b828bac6218a0cd71877d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1383b64ce58d6db338db2ece6b432b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "432b16187008f6662889",
+    "url": "/static/css/main.b80217f8.chunk.css"
+  },
+  {
+    "revision": "23037c46d8eb2b87eb2e",
+    "url": "/static/js/2.517507ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.517507ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432b16187008f6662889",
+    "url": "/static/js/main.a995c4d8.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
